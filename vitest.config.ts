7f2a749f52cb9604abcf9e4b85b6a3a3bbb08451
Fenil Shah <fshah@resourcedata.com>
2026-01-20 11:58:57 -0600

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,16 +1,8 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  plugins: [react()],
   test: {
-    environment: 'node',
+    environment: "node",
     globals: true,
   },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-})
+});
